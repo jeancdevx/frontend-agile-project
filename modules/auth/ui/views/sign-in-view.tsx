0d3567166ed5a92a4e8bf135e0d08601cf12f2bc
Carlos Diaz <cdiazu20@hotmail.com>
2025-09-28 00:44:34 -0500

    fix(auth): update sign-in view to handle error messages

--- modules/auth/ui/views/sign-in-view.tsx
+++ modules/auth/ui/views/sign-in-view.tsx
@@ -32,7 +32,6 @@ import { signInFormSchema } from '../schemas'
 const SignInView = () => {
   const router = useRouter()
 
-  // todo: usar estos estados para manejar la UI
   const [error, setError] = useState<string | null>(null)
   const [pending, setPending] = useState<boolean>(false)
   const [showPassword, setShowPassword] = useState<boolean>(false)
@@ -79,7 +78,7 @@ const SignInView = () => {
                           />
                         </FormControl>
 
-                        {/* todo: Mostrar mensajes de error para credenciales incorrectas */}
+                        <FormMessage />
                       </FormItem>
                     )}
                   />
@@ -95,7 +94,7 @@ const SignInView = () => {
                         <FormControl>
                           <div className='relative'>
                             <Input
-                              type={true ? 'text' : 'password'}
+                              type={showPassword ? 'text' : 'password'}
                               placeholder='********'
                               className='pr-10'
                               {...field}
@@ -105,12 +104,12 @@ const SignInView = () => {
                               variant='ghost'
                               size='sm'
                               className='absolute top-0 right-0 h-full px-3 py-2 hover:bg-transparent'
-                              onClick={() => {}}
+                              onClick={() => setShowPassword(!showPassword)}
                               aria-label={
-                                true ? 'Hide password' : 'Show password'
+                                showPassword ? 'Hide password' : 'Show password'
                               }
                             >
-                              {true ? (
+                              {showPassword ? (
                                 <EyeOffIcon className='h-4 w-4' />
                               ) : (
                                 <EyeIcon className='h-4 w-4' />
@@ -124,22 +123,22 @@ const SignInView = () => {
                   />
                 </div>
 
-                {!!true && (
+                {!!error && (
                   <Alert
                     variant='destructive'
                     className='bg-destructive/10 border-none text-sm'
                   >
                     <OctagonAlertIcon className='h-4 w-4' />
-                    <AlertTitle>{true}</AlertTitle>
+                    <AlertTitle>{error}</AlertTitle>
                   </Alert>
                 )}
 
                 <Button
                   type='submit'
                   className='w-full bg-orange-400 hover:bg-orange-400/90'
-                  disabled={true}
+                  disabled={pending}
                 >
-                  {true ? 'Iniciando sesión...' : 'Iniciar sesión'}
+                  {pending ? 'Iniciando sesión...' : 'Iniciar sesión'}
                 </Button>
 
                 <div className='text-center text-sm'>
@@ -149,7 +148,7 @@ const SignInView = () => {
                       href='/sign-up'
                       className={cn(
                         'font-medium text-orange-600',
-                        true && 'pointer-events-none'
+                        pending && 'pointer-events-none'
                       )}
                     >
                       Regístrate
