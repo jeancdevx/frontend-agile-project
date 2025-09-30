a8909e2a1de4631e727fa4fbe715cd1390d3d7f7
jeancdev <jcode2006@gmail.com>
2025-09-29 22:12:50 -0500

    feat(auth): refactor sign-in view

--- app/(auth)/(routes)/sign-in/page.tsx
+++ app/(auth)/(routes)/sign-in/page.tsx
@@ -1,4 +1,4 @@
-import { SignInView } from '@/modules/auth/ui/views/sign-in-view'
+import { SignInView } from '@/modules/auth/ui/views'
 
 export default function SignInPage() {
   return <SignInView />
